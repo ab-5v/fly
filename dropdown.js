74381066e7bb8d93a74586e858f10b643b657929
Artur Burtsev <artjock@gmail.com>
2014-01-04 02:09:29 +0100

    dropdown draft

--- dropdown.js
+++ dropdown.js
@@ -1,28 +1,43 @@
 ;(function() {
 
-var defaults = {
+function dropdown(handle, options) {
+    return dropdown.fn.create(handle, options);
+};
+
+dropdown.count = 0;
+dropdown.defaults = {
+
     baseClass: 'dropdown',
     hideClass: 'dropdown_hidden',
     extraClass: '',
-    initialHide: true
+    initialHide: true,
+
+    uniqGroup: 'uniq',
+    action: 'click',
+    content: function() {
+        return '<br/>';
+    }
 };
 
-function dropdown(handle, options) {
-    return dropdown.create(handle, options);
-};
-
-$.extend(dropdown, {
+dropdown.fn = {
 
     create: function(handle, options) {
 
-        ctor.prototype = dropdown;
-        return $.extend(new ctor(), {
+        dropdown_.prototype = dropdown.fn;
+        return $.extend(new dropdown_(), {
+            ns: 'ns' + dropdown.count++,
             $root: null,
             $handle: $(handle),
-            options: $.extend({}, defaults, options)
-        });
+            options: $.extend({}, dropdown.defaults, options)
+        }).init();
 
-        function ctor() {}
+        function dropdown_() {}
+    },
+
+    init: function() {
+        this.handle();
+
+        return this;
     },
 
     root: function() {
@@ -41,11 +56,58 @@ $.extend(dropdown, {
     },
 
     destroy: function() {
-        this.$root.remove();
-        this.$root = null;
+        if (this.$root) {
+            this.$root.remove();
+            this.$root = null;
+        }
+    },
+
+    handle: function() {
+        var ns = '.' + this.ns;
+        var action = this.options.action;
+        console.log('handle1', action);
+
+        if (action === 'click') {
+            this.$handle
+                .on('click' + ns, $.proxy(this.toggle, this));
+
+        } else if (action === 'hover') {
+            this.$handle
+                .on('mouseenter' + ns, $.proxy(this.show, this))
+                .on('mouseleave' + ns, $.proxy(this.hide, this))
+
+        } else if (typeof action === 'function') {
+            action()
+        }
+    },
+
+    position: function() {
+        var handle = this.$handle[0].getBoundingClientRect();
+        var dropdown = this.root()[0].getBoundingClientRect();
+        var scroll = {top: $(window).scrollTop(), left: $(window).scrollLeft};
+
+        this.root().css({
+            top: scroll.top + handle.top + handle.height + 10,
+            left: scroll.left + handle.left + (handle.width - dropdown.width)/2
+        });
+    },
+
+    toggle: function() {
+        this[this.root().hasClass(this.options.hideClass) ? 'hide' : 'show']();
+    },
+
+    show: function() {
+        this.root().html( this.options.content() );
+        this.position();
+        this.root().removeClass(this.options.hideClass);
+    },
+
+    hide: function() {
+        this.root()
+            .addClass(this.options.hideClass);
     }
 
-});
+};
 
 var expando = 'dropdown_' + +new Date();
 
