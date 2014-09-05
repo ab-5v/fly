96374a38fac0ea1287cd4f7c36c6c803a51582aa
Artur Burtsev <artjock@gmail.com>
2014-09-05 14:11:14 +0200

    assets reorganisation

---
+++ index.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    $('.js-h1').dropdown({content: '<div>Content</div>'});
+
+    $('.js-h2').tooltip({position: 'left', content: function() { return 'Tooltip'; }});
+    $('.js-h3').tooltip({extraClass: 'tooltip', position: 'right', content: function() { return 'Tooltip'; }});
+    $('.js-h4').dropdown({position: 'top', content: function(done) { setTimeout(function() { done('Tooltip 500'); }, 500); }});
+
+    $('.js-trigger-big').click(function() { $('.js-auto-handle').toggleClass('big'); });
+    $('.js-auto-handle').each(function(i, el) {
+        $(el).dropdown({position: $(el).attr('data-position'), content: 'Menu item 1<br/>Menu item 2'});
+    });
+
+});
