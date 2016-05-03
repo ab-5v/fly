371a534344c94bd68637ad3e6a8c2bdcd17da786
Artur Burtsev <artur.burtsev@booking.com>
2016-05-03 13:25:34 +0200

    CommonJS support (but jQuery still has to be global)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,15 @@ module.exports = function(grunt) {
                         ' * @author <%= bower.authors[0]  %>\n' +
                         ' * @see <%= bower.homepage %>\n' +
                         ' */\n' +
-                        ';(function() {\n',
-                footer: '})();'
+                        ';(function(root, factory) {\n' +
+                        '   if (typeof exports === \'object\') {\n' +
+                        '       module.exports = factory(root.jQuery);\n' +
+                        '   } else {\n' +
+                        '       root.jQuery.fly = factory(root.jQuery);\n' +
+                        '   }\n' +
+                        '})(this, function($) {\n',
+                footer: '   return fly;\n' +
+                        '});'
             },
             full: {
                 src: [
--- fly.full.js
+++ fly.full.js
@@ -4,7 +4,13 @@
  * @author Artur Burtsev <artjock@gmail.com>
  * @see https://github.com/artjock/fly
  */
-;(function() {
+;(function(root, factory) {
+   if (typeof exports === 'object') {
+       module.exports = factory(root.jQuery);
+   } else {
+       root.jQuery.fly = factory(root.jQuery);
+   }
+})(this, function($) {
 
 /**
  * Wrapper
@@ -667,8 +673,6 @@ fly.dropdown = fly._base.extend({
  * @requires _base.js
  */
 
-$.fly = fly;
-
 /**
  * Registers fly component, as jquery plugin
  * @static
@@ -714,4 +718,5 @@ fly.register$ = function(type, component) {
 };
 
 $.each(fly, fly.register$);
-})();
+   return fly;
+});
